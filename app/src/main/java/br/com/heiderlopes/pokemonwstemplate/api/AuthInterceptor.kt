3b3e63950b3b82f510df55ea3dcebf1ab7ff4de2
Helton Isac <helton.isac@gmail.com>
2020-09-17 20:30:37 -0300

    Fixing AuthInterceptor

--- app/src/main/java/br/com/heiderlopes/pokemonwstemplate/api/AuthInterceptor.kt
+++ app/src/main/java/br/com/heiderlopes/pokemonwstemplate/api/AuthInterceptor.kt
@@ -2,16 +2,13 @@ package br.com.heiderlopes.pokemonwstemplate.api
 
 import android.util.Log
 import okhttp3.Interceptor
-import retrofit2.Response
+import okhttp3.Response
 
 class AuthInterceptor : Interceptor {
+
     override fun intercept(chain: Interceptor.Chain?): Response {
-        val requestBuilder = chain!!.request()
-            .newBuilder()
-        requestBuilder.addHeader(
-            "Authorization",
-            "Basic cG9rZWFwaTpwb2tlbW9u"
-        )
+        val requestBuilder = chain!!.request().newBuilder()
+        requestBuilder.addHeader("Authorization", "Basic cG9rZWFwaTpwb2tlbW9u")
         val request = requestBuilder.build()
         val response = chain.proceed(request)
         if (response.code() == 401) {
