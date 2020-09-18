65d3786796c2b3d809942659cf5f2a244747a3fd
Helton Isac <helton.isac@gmail.com>
2020-09-17 21:59:36 -0300

    Creating Pokemon Model and Response

--- app/src/main/java/br/com/heiderlopes/pokemonwstemplate/api/PokemonService.kt
+++ app/src/main/java/br/com/heiderlopes/pokemonwstemplate/api/PokemonService.kt
@@ -1,10 +1,19 @@
 package br.com.heiderlopes.pokemonwstemplate.api
 
 import br.com.heiderlopes.pokemonwstemplate.model.HealthResponse
+import br.com.heiderlopes.pokemonwstemplate.model.PokemonResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface PokemonService {
     @GET("/api/pokemon/health")
     fun checkHealth(): Call<HealthResponse>
+
+    @GET("/api/pokemon")
+    fun getPokemons(
+        @Query("size") size: Int,
+        @Query("sort") sort: String
+    ): Call<PokemonResponse>
+
 }
