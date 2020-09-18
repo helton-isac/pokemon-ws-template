87b5eb3ecfb64b9ae613e7e759dfaddb6d3b18af
Helton Isac <helton.isac@gmail.com>
2020-09-17 21:10:52 -0300

    Including SplashActivity

--- app/src/main/java/br/com/heiderlopes/pokemonwstemplate/view/splash/SplashActivity.kt
+++ app/src/main/java/br/com/heiderlopes/pokemonwstemplate/view/splash/SplashActivity.kt
@@ -1,13 +1,27 @@
 package br.com.heiderlopes.pokemonwstemplate.view.splash
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import br.com.heiderlopes.pokemonwstemplate.R
+import br.com.heiderlopes.pokemonwstemplate.view.main.MainActivity
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class SplashActivity : AppCompatActivity() {
-
+    val splashViewModel: SplashViewModel by viewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        splashViewModel.checkHealth()
+        splashViewModel.messageError.observe(this, Observer {
+            if (it == "") {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            } else {
+                Toast.makeText(this, it, Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
