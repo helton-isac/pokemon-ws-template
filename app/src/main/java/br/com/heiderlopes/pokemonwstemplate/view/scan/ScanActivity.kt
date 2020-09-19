b6a31c370d6247984d0fe1a6b7335ddc17d10b2e
Helton Isac <helton.isac@gmail.com>
2020-09-19 09:56:13 -0300

    Fixing Scan Bug. Missing loading

--- app/src/main/java/br/com/heiderlopes/pokemonwstemplate/view/scan/ScanActivity.kt
+++ app/src/main/java/br/com/heiderlopes/pokemonwstemplate/view/scan/ScanActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.view.View
